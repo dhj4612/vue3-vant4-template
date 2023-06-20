7f3bc390af4b6763c1e8ab2a41f8f18b94dca65c
18781 <18781075421@163.com>
2023-06-20 09:39:24 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import 'nprogress/nprogress.css'
 import App from './App.vue'
 import pinia from './store'
 import router from '@/router'
-
+import 'vant/lib/index.css'; // vant-css
+import VantComponent from 'vant'
 createApp(App)
     .use(pinia)
     .use(router)
+    .use(VantComponent)
     .mount('#app')
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,10 @@
 <template>
   <h1>HelloWorld</h1>
+  <van-button type="primary" block>hello</van-button>
+  <van-date-picker/>
 </template>
 
 <script setup>
-
 </script>
 
 <style scoped>
