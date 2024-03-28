8507910fd1f9c81b55be3d086ccee2002c47da6b
dhj <18781075421@163.com>
2024-03-28 17:47:24 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ import pinia from './store'
 import router from '@/router'
 import 'vant/lib/index.css'; // vant-css
 import VantComponent from 'vant'
+import JsonViewer from 'vue-json-viewer'
+
 createApp(App)
     .use(pinia)
+    .use(JsonViewer)
     .use(router)
     .use(VantComponent)
     .mount('#app')
